e39bfe8605c58787d8e178e4fdf5b96b210b1696
alessandrolega <alessandrolega24@gmail.com>
2022-12-20 00:31:12 +0100

    creata class cibo + attr + construct + parent

---
+++ classi/accessori.php
@@ -0,0 +1,23 @@
+<?php 
+
+    include_once __DIR__ . '/classi/prodotti.php';
+
+    class Accessori{
+        public $materiale;
+        public $dimensioni;
+
+    public function __construct(
+        String $immagine,
+        String $nome,
+        Float $prezzo,
+        Category $category,
+        String $materiale,
+        String $dimensioni
+        ){
+        $this->materiale = $materiale;
+        $this->dimensioni = $dimensioni;
+        parent::__construct($immagine, $nome, $prezzo, $category)
+    }
+    }
+
+?>
