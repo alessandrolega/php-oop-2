7f023b85adf2dbe7ef2051746d1cb66ac8240a40
alessandrolega <alessandrolega24@gmail.com>
2022-12-19 23:42:20 +0100

    aggiunto include once di Category

--- classi/prodotti.php
+++ classi/prodotti.php
@@ -1,12 +1,19 @@
 <?php 
 
+    include_once __DIR__ . '/classi/category.php';
+
     class Prodotto{
         public $immagine;
         public $nome;
         public $prezzo;
         public $category;
 
-    public function __construct($immagine, $nome, $prezzo, $category){
+    public function __construct(
+        String $immagine,
+        String $nome,
+        Float $prezzo,
+        Category $category
+        ){
         $this->immagine = $immagine;
         $this->nome = $nome;
         $this->prezzo = $prezzo;
